3de2b1da9c8ac1405fdccfbddc8ba702fb5f286d
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-05-27 21:01:36 +0530

    [jayesh] In user registration FirstName

---
+++ RegexProject/Paterns.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProject
+{
+
+    internal class Paterns
+    {
+        Regex regex = new Regex("^[A-z][1][A-Za-z]{2,}$");
+
+        public void ValidFirstName(string First_Name)
+        {
+            Console.WriteLine("Enter FirstName:" +First_Name);
+            if (regex.IsMatch(First_Name))
+            {
+                Console.WriteLine("valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+       
+
+    }
+}
---
+++ RegexProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProject 
+  {
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Paterns paterns = new Paterns();
+            Console.WriteLine("Enter First Name:");
+            string firstName=Console.ReadLine();    
+            paterns.ValidFirstName(firstName);  
+        }
+    }
+    
+  }
